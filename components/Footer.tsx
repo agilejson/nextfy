f23009db947ab5fd9bfb338a65dba2cb8011717e
agile <toptal.powerful@gmail.com>
2024-07-18 17:44:48 -0300

    feat: terms and privacy policies pages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,10 @@ export function Footer() {
       <div className="m-auto flex h-16 w-full max-w-screen-wrapper items-center justify-between">
         <span className="text-sm text-neutral-300">© 2024 | Nextfy | Todos os direitos reservados.</span>
         <div className="flex gap-6">
-          <Link href="#" className="hover:underline">
-            Política de Privacidade
+          <Link href="/privacy" className="hover:underline">
+            Política de privacidade
           </Link>
-          <Link href="#" className="hover:underline">
-            Termos de Uso
-          </Link>
-          <Link href="#" className="hover:underline">
+          <Link href="/terms" className="hover:underline">
             Termos de Uso
           </Link>
         </div>
