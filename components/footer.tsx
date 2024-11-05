0efe5818aca35fb30ba100662fe7fb2b86a1c076
agile <toptal.powerful@gmail.com>
2024-11-05 11:27:47 -0300

    style: footer responsiveness

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,9 @@ export function Footer() {
   return (
     <footer className="mt-32 border-t border-black">
       <Wrapper>
-        <div className="flex h-16 w-full items-center justify-between">
-          <span className="text-sm">© 2024 | {SITE_NAME} | Todos os direitos reservados.</span>
-          <div className="flex gap-6">
+        <div className="flex w-full items-center justify-between py-4 tablet:flex-col-reverse tablet:gap-4">
+          <span className="text-sm tablet:w-full">© 2024 | {SITE_NAME} | Todos os direitos reservados.</span>
+          <div className="flex gap-6 tablet:w-full tablet:flex-col tablet:gap-2 tablet:text-sm">
             <Link href="/legal/privacy-policy" className="hover:underline">
               Política de privacidade
             </Link>
