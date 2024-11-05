f44ff452b92024f9f61ad85afc7c5dfefc741063
agile <toptal.powerful@gmail.com>
2024-11-05 11:15:44 -0300

    style: carousel responsiveness

--- components/product/carousel.tsx
+++ components/product/carousel.tsx
@@ -14,13 +14,13 @@ export async function Carousel({ collection }: CollectionProps) {
 
   return (
     <div className="flex w-full flex-col gap-4">
-      <div className="flex w-full justify-between">
-        <span className="text-2xl">{data.title}</span>
-        <Link href={`/collections/${data.title.toLowerCase()}`} className="font-semibold">
+      <div className="item flex w-full items-center justify-between">
+        <span className="w-max text-2xl mobile:text-base tablet:text-xl">{data.title}</span>
+        <Link href={`/collections/${data.title.toLowerCase()}`} className="font-semibold tablet:text-sm">
           Ver todos
         </Link>
       </div>
-      <ul className="relative flex w-full gap-5 overflow-x-scroll">
+      <ul className="relative flex w-full gap-5 overflow-x-scroll tablet:gap-3">
         {data.products.map((product) => {
           const minPrice = product.priceRange.minVariantPrice
           const compareAtPrice = product.compareAtPriceRange.maxVariantPrice
@@ -29,21 +29,22 @@ export async function Carousel({ collection }: CollectionProps) {
           return (
             <li key={product.id}>
               <Link href={productUrl}>
-                <div className="relative flex h-[270px] w-[270px] items-center justify-center border border-black bg-white p-4">
+                <div className="relative flex h-[270px] w-[270px] items-center justify-center border border-black bg-white p-4 tablet:h-[200px] tablet:w-[200px]">
                   <Image
                     src={product.featuredImage?.url}
                     alt={product.title}
                     fill
                     sizes="270px"
-                    style={{ objectFit: 'contain', padding: '16px' }}
+                    style={{ objectFit: 'contain' }}
+                    className="p-4 tablet:p-6"
                   />
                 </div>
                 <div className="flex flex-col gap-2 py-2">
-                  <span className="flex w-[270px] text-balance">{product.title}</span>
-                  <div className="flex gap-2">
-                    <span className="text-lg font-bold">{formatPriceToBrl(minPrice.amount)}</span>
+                  <span className="flex w-[270px] text-balance tablet:w-[200px] tablet:text-sm">{product.title}</span>
+                  <div className="flex w-full gap-2">
+                    <span className="text-lg font-bold tablet:text-base">{formatPriceToBrl(minPrice.amount)}</span>
                     {compareAtPrice.amount > minPrice.amount && (
-                      <span className="line-through">{formatPriceToBrl(compareAtPrice.amount)}</span>
+                      <span className="line-through tablet:text-xs">{formatPriceToBrl(compareAtPrice.amount)}</span>
                     )}
                   </div>
                 </div>
